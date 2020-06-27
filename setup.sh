60054d1b01a90695ffd25728f5fa0d13494f277b
MacBook Pro <izuruk@gmail.com>
2020-06-27 12:28:24 +0900

    echoをクオーティング

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if ! which brew >& /dev/null;then
-  echo Homebrew is not installed!
-  echo Install now...
-  echo /bin/bash -c \"\$\(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh\)\"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo
+	echo "Homebrew is not installed!"
+	echo "Install now..."
+	echo /bin/bash -c \"\$\(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh\)\"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	echo
 fi
 
 DOT_FILES=(.Brewfile .vimrc)
@@ -17,5 +17,3 @@ done
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
